912f78567d144bc894669bfcada3f5221fc55ccd
kiprono Denis <kipronokemei98@gmail.com>
2020-07-03 19:32:10 +0300

    Add tips

--- src/Components/Container.vue
+++ src/Components/Container.vue
@@ -2,13 +2,16 @@
 
 <div>
   <h1 class="text-3xl text-purple-500 font-bold ">30 Things You Need To Know About Landing Page</h1>
-<ul class="list-unstyled  m-4 lg:mx-10 w-full lg:w-3/4 lg:mx-auto">
+<ul class="list-unstyled lg:mx-10 w-full lg:w-3/4 lg:mx-auto mb-10">
   <li v-for="tip in tips" :key="tip.id">
     <h4 class="my-3 text-xl font-bold">{{tip.id}}. {{tip.title}}</h4>
     <div v-html="tip.description" class="text-gray-500"></div>
   </li>
 </ul>
 
+<footer class="bg-black text-blue-800 py-10">
+  <a href="http://www.kipronodeno.com/" class="m-4 w-2/5 p-4 bg-blue-500">Find Landing Page Designer Here.</a>
+</footer>
 </div>
 </template>
 
